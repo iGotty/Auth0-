f15175a7d35ced79c2ace32836b7a1396e121a80
Gotty <78111224+iGotty@users.noreply.github.com>
2023-11-16 23:07:14 -0500

    Update views.py

--- activos/views.py
+++ activos/views.py
@@ -4,14 +4,23 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .logic.logic_activo import create_activo, get_activos
+from django.contrib.auth.decorators import login_required
+from monitoring.auth0backend import getRole
 
+@login_required
 def activo_list(request):
-    activos = get_activos()
-    context = {
-        'activo_list': activos
-    }
-    return render(request, 'Activo/activos.html', context)
+    role = getRole(request)
+    if role == "Financiero":
+        activos = get_activos()
+        context = {
+            'activo_list': activos
+        }
+        return render(request, 'Activo/activos.html', context)
+    
+    else:
+        return HttpResponse("Unauthorized User")
 
+@login_required
 def activo_create(request):
     if request.method == 'POST':
         form = ActivoForm(request.POST)
@@ -29,3 +38,4 @@ def activo_create(request):
     }
 
     return render(request, 'Activo/activoCreate.html', context)
+
